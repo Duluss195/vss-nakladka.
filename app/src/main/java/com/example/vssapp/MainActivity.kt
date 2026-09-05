5f2447383630a3d8fa2b299c645b8b09f35c7969
Duluss195 <jan_dulewicz@o2.pl>
2026-09-05 19:59:48 +0200

    apk

--- app/src/main/java/com/example/vssapp/MainActivity.kt
+++ app/src/main/java/com/example/vssapp/MainActivity.kt
@@ -1,10 +1,28 @@
 package com.example.vssapp
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Sprawdzamy zgode na wyswietlanie nad innymi aplikacjami
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivity(intent)
+        } else {
+            // Startujemy usluge nakladki
+            val intent = Intent(this, OverlayService::class.java)
+            startService(intent)
+        }
     }
 }
